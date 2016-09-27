bc5110036a7ea327d15a6b0bcd7c77bbeb08146e
Itiel Beeri <itiel.beeri@gmail.com>
2016-09-27 14:34:29 +0300

    Lesson 01

---
+++ 01 - GettingStartedWithEF/GettingStartedWithEF/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedWithEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ProductsContext())
+            {
+                foreach (var p in context.Products.Where(p=>p.ProductName.StartsWith("a")))
+                {
+                    Console.WriteLine(p.ProductName + " in category " + p.Category.CategoryName);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
